aa739aaec0d80ea1ba959c8bdf01d77b4eae9b64
yanfi-sher <loss.soul20@gmail.com>
2023-11-05 14:32:09 +0200

    first commit

---
+++ src/Calculations.java
@@ -0,0 +1,15 @@
+public class Calculations {
+
+    public static double sum(double op1,double op2){
+        return op1+op2;
+    }
+    public static double subtract(double op1,double op2){
+        return op1-op2;
+    }
+    public static double division(double op1,double op2){
+        return op1/op2;
+    }
+    public static double multiply(double op1,double op2){
+        return op1*op2;
+    }
+}
