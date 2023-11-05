2edf9ba2ca9062127989333a2478f9a1ee413593
yanfi-sher <loss.soul20@gmail.com>
2023-11-05 14:32:46 +0200

    second commit

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,5 +1,10 @@
 public class Calculations {
-
+    /**
+     *
+     * @param op1
+     * @param op2
+     * @return sum of op1 and op2
+     */
     public static double sum(double op1,double op2){
         return op1+op2;
     }
